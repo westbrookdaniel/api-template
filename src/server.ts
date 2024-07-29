11381f8a40074e4a4ea8f34664c6bab330674051
westy12dan <westy12dan@gmail.com>
2024-07-29 23:57:19 +0800

    Improve fly deploy

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,10 @@ import app from './app';
 const fastify = await app();
 
 try {
-  await fastify.listen({ port: parseInt(process.env.PORT || '3000') });
+  await fastify.listen({
+    port: parseInt(process.env.PORT || '3000'),
+    host: '0.0.0.0',
+  });
 } catch (err) {
   fastify.log.error(err);
   process.exit(1);
